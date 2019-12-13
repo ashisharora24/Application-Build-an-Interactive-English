2a16cf829b29e49514375a4a8769d8d51287f55e
Ashish Arora <ashisharora24@gmail.com>
2019-12-13 10:53:04 +0530

    taking input and returning then meaning

--- application.py
+++ application.py
@@ -1,1 +1,10 @@
-# only for testing 
+# only for testing
+import json
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    return data[word]
+
+word = input("Enter the word : ")
+print(translate(word))
