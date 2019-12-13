389451886273e4ce1f6fd631c84f73ff4ee95b02
Ashish Arora <ashisharora24@gmail.com>
2019-12-13 10:55:56 +0530

    added return statement if word not found

--- application.py
+++ application.py
@@ -4,7 +4,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    return "The word {} doesnot exist. Please double check it.".format(word)
+
 
 word = input("Enter the word : ")
 print(translate(word))
